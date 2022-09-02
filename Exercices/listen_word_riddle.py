044544123298a04abf85aee1b41a25c696da9613
BizMaz <gabriel.mariethoz@edu.gfbienne.ch>
2022-09-02 15:42:31 +0200

    end_option of print function

--- Exercices/listen_word_riddle.py
+++ Exercices/listen_word_riddle.py
@@ -3,4 +3,4 @@ ALPHABET = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
 MESSAGE = [12, 9, 19, 20, 0, 18, 9, 4, 4, 12, 5]
 
 for index in MESSAGE:
-    print(ALPHABET[index])
+    print(ALPHABET[index], end=' ')
