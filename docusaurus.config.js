5ddcf53a861434396cf74fa1b78b9e0197a689bb
BizMaz <gabriel.mariethoz@edu.gfbienne.ch>
2022-11-12 10:30:43 +0100

    das favicon zu logo.ico geändert

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   baseUrl: `/${REPO_NAME}/`,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/logo.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
